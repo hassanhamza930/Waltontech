c642487cc3db110e4bd00464c5663b6901e968db
hassanhamza930 <hassanhamza930@gmail.com>
2023-03-11 03:35:26 +0500

    How we work section added.

---
+++ src/pages/landingPage/components/ourworkflow.tsx
@@ -0,0 +1,78 @@
+function OurWorkflow() {
+    return ( 
+        <div id="whoweare" className=" bg-blue/90  flex flex-col h-full justify-center items-center w-full">
+                <div className="bg-blue bg-contain bg-blend-lighten bg-right-top flex bg-no-repeat flex-col h-full justify-center items-center w-full py-0 md:py-10">
+                    <div className="w-full xl:w-[50%] bg-blue/70 md:bg-transparent rounded-sm flex flex-col justify-start items-start p-6 md:p-10 gap-4">
+
+                        <div className="flex flex-row justify-center items-center gap-4">
+                            <div className="w-12 h-[3px] rounded-full bg-tan/90"></div>
+                            <div className="text-sm font-regular text-tan/90">How we work</div>
+                        </div>
+
+                        <div className="flex justify-start items-start flex-col gap-5">
+                            <div className="text-4xl md:text-5xl font-bold text-tan/90">Our Workflow for End to End Success</div>
+
+                            <div className="flex h-auto w-full flex-row justify-start items-start gap-2 mt-10">
+                                <div className="flex-1 flex-col justify-start items-center py-3">
+                                    <div className="h-3 w-3 rounded-full bg-tan"></div>
+                                    <div className="h-full w-2 bg-tan/90 "></div>
+                                </div>
+                                <div className="flex flex-col justify-start items-start gap-4">
+                                    <div className="text-3xl md:text-4xl font-bold text-tan/90 -mt-[3px]">Discussion</div>
+                                    <div className="text-md font-regular text-tan/90">
+                                        At the company, communication is considered essential for successful projects. During the initial consultation, the team listens to clients, asks questions, and gathers all necessary details about the project requirements. This information is then used to create a customized development plan that meets clients' specific needs. The team also offers professional expertise and insights, including advice and recommendations on the best technologies and approaches based on their experience and industry best practices.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="flex h-auto w-full flex-row justify-start items-start gap-2 mt-10">
+                            <div className="flex-1 flex-col justify-start items-center py-3">
+                                <div className="h-3 w-3 rounded-full bg-tan"></div>
+                                <div className="h-full w-2 bg-tan/90 "></div>
+                            </div>
+                            <div className="flex flex-col justify-start items-start gap-4">
+                                <div className="text-3xl md:text-4xl font-bold text-tan/90 -mt-[3px]">Onboarding</div>
+                                <div className="text-md font-regular text-tan/90">
+                                    The client will be onboarded onto project management and design tools such as Jira and Figma to enable real-time collaboration, progress tracking and information sharing. Figma will be used to share designs, gather feedback and make revisions in real-time, ensuring the client's involvement in the design process and meeting their expectations for the final product.
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="flex h-auto w-full flex-row justify-start items-start gap-2 mt-10">
+                            <div className="flex-1 flex-col justify-start items-center py-3">
+                                <div className="h-3 w-3 rounded-full bg-tan"></div>
+                                <div className="h-full w-2 bg-tan/90 "></div>
+                            </div>
+                            <div className="flex flex-col justify-start items-start gap-4">
+                                <div className="text-3xl md:text-4xl font-bold text-tan/90 -mt-[3px]">Design</div>
+                                <div className="text-md font-regular text-tan/90">
+                                    Our software services company offers a range of design services, including UX/UI design, graphic design, and branding. We work closely with clients to understand their needs and deliver designs that align with their brand identity and business goals. The company takes a user-centric approach to design, creating intuitive and user-friendly interfaces to enhance the user experience.                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="flex h-auto w-full flex-row justify-start items-start gap-2 mt-10">
+                            <div className="flex-1 flex-col justify-start items-center py-3">
+                                <div className="h-3 w-3 rounded-full bg-tan"></div>
+                                <div className="h-full w-2 bg-tan/90 "></div>
+                            </div>
+                            <div className="flex flex-col justify-start items-start gap-4">
+                                <div className="text-3xl md:text-4xl font-bold text-tan/90 -mt-[3px]">Development & Delivery</div>
+                                <div className="text-md font-regular text-tan/90">
+                                    Our web development agency follows a structured approach to managing projects, which involves gathering requirements, creating a detailed project plan and using agile development methodologies. We prioritize tasks and regularly communicate with clients to ensure their needs are being met. The development team places a strong emphasis on collaboration and knowledge sharing to ensure everyone is on the same page. The overall process is designed to ensure efficient, effective, and high-quality delivery of projects.                            </div>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+            </div>
+     );
+}
+
+export default OurWorkflow;
--- src/pages/landingPage/landingPage.tsx
+++ src/pages/landingPage/landingPage.tsx
@@ -9,6 +9,7 @@ import HeroSection from "./components/herosection";
 import WhatWeDo from "./components/whatwedo";
 import WhoWeAre from "./components/whoweare";
 import OurTechStack from "./components/ourtechstack";
+import OurWorkflow from "./components/ourworkflow";
 
 function LandingPage() {
 
@@ -18,46 +19,8 @@ function LandingPage() {
             <HeroSection />
             <WhatWeDo />
             <WhoWeAre />
-            <OurTechStack />
-
-
-            <div id="whoweare" className=" bg-blue/90  flex flex-col h-full justify-center items-center w-full">
-                <div className="bg-blue bg-contain bg-blend-lighten bg-right-top flex bg-no-repeat flex-col h-full justify-center items-center w-full py-0 md:py-10">
-                    <div className="w-full xl:w-[50%] bg-blue/70 md:bg-transparent rounded-sm flex flex-col justify-start items-start p-6 md:p-10 gap-4">
-
-                        <div className="flex flex-row justify-center items-center gap-4">
-                            <div className="w-12 h-[3px] rounded-full bg-tan/90"></div>
-                            <div className="text-sm font-regular text-tan/90">How we work</div>
-                        </div>
-
-                        <div className="flex justify-start items-start flex-col gap-5">
-                            <div className="text-4xl md:text-5xl font-bold text-tan/90">Our Workflow for End to End Success</div>
-
-                            <div className="flex h-auto w-full flex-row justify-start items-start gap-2 mt-10">
-                                <div className="flex-1 flex-col justify-start items-center py-3">
-                                    <div className="h-3 w-3 rounded-full bg-tan"></div>
-                                    <div className="h-full w-2 bg-tan/90 "></div>
-                                </div>
-                                <div className="flex flex-col justify-start items-start gap-4">
-                                    <div className="text-3xl md:text-4xl font-bold text-tan/90 -mt-[3px]">Design</div>
-                                    <div className="text-md font-regular text-tan/90">
-                                    At the company, communication is considered essential for successful projects. During the initial consultation, the team listens to clients, asks questions, and gathers all necessary details about the project requirements. This information is then used to create a customized development plan that meets clients' specific needs. The team also offers professional expertise and insights, including advice and recommendations on the best technologies and approaches based on their experience and industry best practices.
-                                    </div>
-                                </div>
-                            </div>
-
-
-                        </div>
-
-
-
-
-                    </div>
-                </div>
-            </div>
-
-
-
+            {/* <OurTechStack /> */}
+            <OurWorkflow/>
         </div>
     );
 }
