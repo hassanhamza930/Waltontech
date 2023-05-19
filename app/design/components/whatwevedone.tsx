6ff66d29783ead3ca8b3fb2b0a6d22082916ef70
Hamza Hassan <hassanhamza930@gmail.com>
2023-05-19 05:49:24 +0500

    What we've done section added.

--- app/design/components/whatwevedone.tsx
+++ app/design/components/whatwevedone.tsx
@@ -2,15 +2,24 @@
 
 import { MdArrowDropDown } from "react-icons/md";
 import { Disclosure, Transition } from '@headlessui/react';
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, ReactElement, useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { isBlackBackgroundAtom } from "../atoms";
 import { useInView } from "react-intersection-observer";
 import { useRecoilState } from "recoil";
+import Image, { StaticImageData } from "next/image";
+import transformationlabs from "../../../images/design/transformationlabs.png";
+import wavehr from "../../../images/design/wavehr.png";
+import cn from "../../../images/design/cn.png";
+import pan from "../../../images/design/pan.png";
+
+import { TbExternalLink } from "react-icons/tb";
 
 interface CompanyCardProps {
     title: string,
     description: string,
+    website?: string,
+    img: StaticImageData
 
 }
 
@@ -26,7 +35,38 @@ function CompanyCard(props: CompanyCardProps) {
             }}
             transition={{ duration: 1, delay: 0.2 }}
             whileInView={"visible"}
-            className="h-96 w-[47%] bg-black rounded-xl">
+            className="relative z-0 h-96 md:h-[600px] w-full transition-all duration-300 rounded-xl overflow-hidden shadow-xl">
+            
+            <div className="absolute z-10 w-full h-full object-cover object-center bg-gradient-to-b from-black to-black/0">
+            </div>
+
+            <div className="flex flex-col justify-start items-start h-full w-full">
+
+
+
+                <div className="z-20 flex flex-col md:flex-row w-full justify-start items-start md:justify-between md:items-center">
+                    <div className="text-2xl font-medium text-tan/90 px-6 md:pl-10 mt-6">
+                        {props.title}
+                    </div>
+                    {
+                        props.website != undefined &&
+                        <button onClick={() => { window.open(props.website) }} className="md:flex hidden gap-2 shadow-xl hover:scale-[1.02] transition-all duration-300 flex-row justify-center items-center text-md bg-tan/90 rounded-full px-12 py-3 font-medium text-black/90 mr-10 mt-6">
+                            Website
+                            <TbExternalLink />
+                        </button>
+                    }
+                </div>
+                <div className="text-tan/90 z-20 text-md font-light px-6 md:px-10 mt-1">
+                    {props.description}
+                </div>
+
+                <div style={{ backgroundImage: `url('${props.img.src}')` }} className="bg-cover bg-center h-full w-full flex justify-center items-center mt-10">
+
+                </div>
+
+            </div>
+
+
 
         </motion.div>
     )
@@ -69,10 +109,12 @@ function Whatwvedone() {
 
 
 
-                <div className="flex flex-wrap w-full h-full justify-between items-start mt-10">
-                            
-                            <CompanyCard title="Careernetwork.co" description="A career focused social media app ."/>
-                            <CompanyCard title="PAN Coin" description="A landing page for a meme coin"/>
+                <div className="flex flex-col w-full h-full justify-between items-start md:mt-16 gap-10">
+
+                    <CompanyCard img={cn} website="https://careernetwork.co" title="Careernetwork.co" description="Design & Development of a career focused social media app ." />
+                    <CompanyCard img={wavehr} website="https://wavehr.vercel.app" title="WaveHR" description="Design and Development for a recruitment based SaaS app." />
+                    <CompanyCard img={pan} title="PAN Coin" description="Landing page for a meme coin." />
+                    <CompanyCard img={transformationlabs} title="TransformationLabs.io" description="Design Revamp for a product management consulting company from Canada." />
 
                 </div>
 
